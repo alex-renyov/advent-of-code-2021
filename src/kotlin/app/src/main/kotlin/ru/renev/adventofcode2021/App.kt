458e9081bfb5167844041dd8a92f3a9f48a37bdc
Alexander Renev <alexander@renev.dev>
2021-12-04 17:42:21 +0300

    Day 2 kotlin

--- src/kotlin/app/src/main/kotlin/ru/renev/adventofcode2021/App.kt
+++ src/kotlin/app/src/main/kotlin/ru/renev/adventofcode2021/App.kt
@@ -3,4 +3,7 @@ package ru.renev.adventofcode2021
 fun main() {
     println(Day1().part1())
     println(Day1().part2())
+
+    println(Day2().part1())
+    println(Day2().part2())
 }
---
+++ src/kotlin/app/src/main/kotlin/ru/renev/adventofcode2021/Day2.kt
@@ -0,0 +1,49 @@
+package ru.renev.adventofcode2021
+
+class Day2 {
+    fun part1() : Int {
+        println("Day 2 part 1")
+
+        val parts = Day2Input.value.split("\n")
+        var pos = 0
+        var depth = 0
+        for (command in parts) {
+            val (action, amount) = command.split(" ")
+            val amountValue = amount.toInt()
+
+            when (action) {
+                "forward" -> pos += amountValue
+                "down"    -> depth += amountValue
+                "up"      -> depth -= amountValue
+                else      -> throw RuntimeException("Invalid action $action")
+            }
+        }
+
+        return pos * depth
+    }
+
+    fun part2() : Int {
+        println("Day 2 part 2")
+
+        val parts = Day2Input.value.split("\n")
+        var pos = 0
+        var depth = 0
+        var aim = 0
+        for (command in parts) {
+            val (action, amount) = command.split(" ")
+            val amountValue = amount.toInt()
+
+            when (action) {
+                "forward" -> {
+                    pos += amountValue
+                    depth += aim * amountValue
+                }
+                "down"    -> aim += amountValue
+                "up"      -> aim -= amountValue
+                else      -> throw RuntimeException("Invalid action $action")
+            }
+        }
+
+        return pos * depth
+    }
+}
