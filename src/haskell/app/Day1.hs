3b8664604045b4ccacffb139b07f6eb07556b27c
Alexander Renev <alexander@renev.dev>
2021-12-07 15:04:53 +0300

    Code beeautify

--- src/haskell/app/Day1.hs
+++ src/haskell/app/Day1.hs
@@ -1,5 +1,6 @@
 module Day1(part1, part2) where
 
+import Data.Functor
 import Day1Input(inputValue)
 
 part1 :: Int
@@ -7,8 +8,8 @@ part1 = increasesCount inputNumbers
 
 part2 :: Int
 part2 = increasesCount triplets
-            where sumLists = zipWith (+)
-                  triplets = foldr1 sumLists $ map drop [0..2] <*> [inputNumbers]
+            where sumLists = foldr1 $ zipWith (+)
+                  triplets = sumLists $ [0..2] <&> flip drop inputNumbers
 
 increasesCount :: [Int] -> Int
 increasesCount values = length $ filter id $ zipWith (<) values $ drop 1 values
